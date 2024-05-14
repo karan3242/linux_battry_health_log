38eab87566bdaec53decc88c3639820f4c4db474
Karan Desai <61599200+karan3242@users.noreply.github.com>
2024-05-14 22:29:53 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 now=$(cat /sys/class/power_supply/BAT0/energy_full)
-
 full=$(cat /sys/class/power_supply/BAT0/energy_full_design)
-
 bat=$(cat /sys/class/power_supply/BAT0/capacity)
 
 div(){
 	echo "scale=3; ($now / $full) * 100" | bc
 }
-
 
 maxcap(){
 	echo "scale=3; $(div) * .8" | bc
@@ -21,6 +18,5 @@ truecap(){
 
 day=$(date "+%D")
 time=$(date "+%T")
-
 
 echo "$day,$time,$(div),$(truecap),$bat" >> /home/kd/opt/bat_helth_log/log.csv
